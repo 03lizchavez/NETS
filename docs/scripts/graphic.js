cbbb77f9a6fbd87cc25b56c687b9fca284acb51a
Nate Hammer <nhammer514@gmail.com>
2024-10-08 17:49:13 -0400

    circle graphic

---
+++ docs/scripts/graphic.js
@@ -0,0 +1,6 @@
+const element = document.getElementById("circle_graphic");
+const text = document.getElementById("graphic_text");
+
+function showText() {
+    text.setAttribute("visibility", "hidden");
+};
