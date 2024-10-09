60f4e6882e1ce9d0e7e0ef80ec38628764300d9b
Nate Hammer <nhammer514@gmail.com>
2024-10-09 17:07:33 -0400

    added dropdown policy tabs

---
+++ docs/scripts/dropdown.js
@@ -0,0 +1,16 @@
+function displayTab(button_id) {
+    var dropdown_element = document.getElementById(String(button_id)).parentNode;
+    var children_elements = dropdown_element.children;
+    if (dropdown_element.style.height == "7vh"){
+        dropdown_element.style.height = "75vh";
+        for (var i = 1; i < children_elements.length; i++){
+            children_elements[i].style.display = "block";
+        };
+    }
+    else{
+        dropdown_element.style.height = "7vh";
+        for (var i = 1; i < children_elements.length; i++){
+            children_elements[i].style.display = "none";
+        };
+    };
+};
