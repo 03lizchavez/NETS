8388bad75762ff268a62b6d21f432e6e5c103887
Nate Hammer <nhammer514@gmail.com>
2024-10-08 20:19:15 -0400

    fixed javascript and added functional text

--- docs/scripts/graphic.js
+++ docs/scripts/graphic.js
@@ -1,6 +1,19 @@
-const element = document.getElementById("circle_graphic");
-const text = document.getElementById("graphic_text");
-
-function showText() {
-    text.setAttribute("visibility", "hidden");
+function showText(element_id) {
+    var selected = document.getElementById(element_id);
+    
+    
+    var text_of_steps = document.getElementsByClassName("graphic_text");
+    for (var i = 0; i < text_of_steps.length; i++){
+        text_of_steps[i].setAttribute("visibility", "hidden");
+    };
+    
+    
+    var graphics = document.getElementsByClassName("circle_graphic")
+    for (var i = 0; i < graphics.length; i++){
+        graphics[i].classList.remove("circle_select");;
+    };
+    
+    
+    selected.firstElementChild.classList.add("circle_select");
+    document.getElementById(String(element_id + "-text")).setAttribute("visibility", "visible");
 };
