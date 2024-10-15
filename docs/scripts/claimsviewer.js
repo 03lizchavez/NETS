999f855f972ef097e31d536618342de3ede6a7aa
nhammer514 <nhammer514@gmail.com>
2024-10-15 10:44:49 -0400

    crunch changes

--- docs/scripts/claimsviewer.js
+++ docs/scripts/claimsviewer.js
@@ -3,9 +3,10 @@ function viewClaim(claim_number_id){
     var claim_number = claim_number_id.split("-");
     var selected_claim_node = document.getElementById(claim_number_id).parentNode;
     
-    deselectClaimSteps()
+    deselectClaimSteps();
     progress_graph.setAttribute('visibility','visible');
     progress_graph.getElementById("claim-number-text").innerHTML = "Claim #" + claim_number[1];
-    progress_graph.getElementById("claim-step2-text").lastChild.innerHTML = selected_claim_node.parentNode.childNodes[5].innerHTML + " is reviewing your claim";
-    progress_graph.getElementById("claim-step1-text").lastChild.innerHTML = "Your claim was filed on" + selected_claim_node.parentNode.childNodes[3].innerHTML;
+    progress_graph.getElementById("claim-step2-text").lastChild.innerHTML = document.getElementById(claim_number_id).getAttribute("data-agent") + " is now reviewing your claim";
+    progress_graph.getElementById("claim-step1-text").lastChild.innerHTML = "Your claim was filed on " + document.getElementById(claim_number_id).getAttribute("data-filing-date");
+
 }
